b98fa5e6a58704db6b4bf962e7726132892b75b9
PrachiiiiisHere <prachhiiii16@gmail.com>
2026-03-15 07:59:42 +0000

    Add initial website files and assets

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+const sections = document.querySelectorAll(".info");
+
+function revealSections(){
+
+    sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const triggerPoint = window.innerHeight * 0.85;
+    if(sectionTop < triggerPoint){
+    section.classList.add("show");
+}
+
+});
+
+}
+
+window.addEventListener("scroll", revealSections);
